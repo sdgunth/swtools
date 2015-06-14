170682a3595c64d4a72a58af3cc7c4679f0d5fa3
lemery <luke.a.emery@gmail.com>
2015-06-14 13:50:03 -0700

    Ugh

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,30 @@ species_array.each do |i|
   bio = i['biology']
   rarity = i['rarity and origin']
     
-  Species.create(
-    :name => i['name'],
-    :social_status => i['social_status'],
-    :size => bio['size'],
-    :diet => bio['diet'],
-    :genders => bio['genders'],
-    :males_per_female => bio['males per female'],
-    :force_sensitivity => bio['force sensitivity'],
-    :lifespan => bio['lifespan'],
-    :home_region => rarity['home region'],
-    :home_region_rarity => rarity['home rarity'],
-    :rarities_by_region => {
-      "Deep Core" => rarity['deep core'],
-      "Core Worlds" => rarity['core worlds'],
-      "Colonies" => rarity['colonies'],
-      "Inner Rim" => rarity['inner rim'],
-      "Expansion Region" => rarity['expansion region'],
-      "Mid Rim" => rarity['mid rim'],
-      "Outer Rim Territories" => rarity['outer rim territories'],
-      "Wild Space" => rarity['wild space'],
-      "Tingel Arm" => rarity['tingel arm'],
-      "Unknown Regions" => rarity['unknown regions']
-    },
-    :wiki_link => 'http://starwars.wikia.com/wiki/' + i['name']
-  ) 
+  new_spec = Species.new
+  puts "Adding name"
+  new_spec[:name] = i['name']
+    puts "Adding social status"
+  new_spec[:social_status] = i['social_status']
+  new_spec[:size] = bio['size']
+  new_spec[:diet] = bio['diet']
+  new_spec[:genders] = bio['genders']
+  new_spec[:males_per_female] = bio['males per female']
+  new_spec[:force_sensitivity] = bio['force sensitivity']
+  new_spec[:lifespan] = bio['lifespan']
+  new_spec[:home_region] = rarity['home region']
+  new_spec[:home_region_rarity] = rarity['home rarity']
+  new_spec[:rarities_by_region] = {
+    "Deep Core" => rarity['deep core'],
+    "Core Worlds" => rarity['core worlds'],
+    "Colonies" => rarity['colonies'],
+    "Inner Rim" => rarity['inner rim'],
+    "Expansion Region" => rarity['expansion region'],
+    "Mid Rim" => rarity['mid rim'],
+    "Outer Rim Territories" => rarity['outer rim territories'],
+    "Wild Space" => rarity['wild space'],
+    "Tingel Arm" => rarity['tingel arm'],
+    "Unknown Regions" => rarity['unknown regions']
+  }
+  new_spec[:wiki_link] = 'http://starwars.wikia.com/wiki/' + i['name']
 end
