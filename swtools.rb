34090765c531bdf7b4d081c23bae72cb21637cd1
lemery <luke.a.emery@gmail.com>
2015-06-17 00:04:02 -0700

    Bugfix

--- swtools.rb
+++ swtools.rb
@@ -27,7 +27,7 @@ end
 require './models/Species'
 
 before do
-  @rarity_coefficients = {"Ubiquitous" => 576.0, "Pervasive" => 384.0, "Common" => 192.0, "Uncommon" => 48.0, "Rare" => 16.0, "Super Rare" => 8.0, "Near-Mythical" => 1.0}
+  @rarity_coefficients = {"Ubiquitous" => 768.0, "Pervasive" => 384.0, "Common" => 192.0, "Uncommon" => 48.0, "Rare" => 16.0, "Super Rare" => 8.0, "Near-Mythical" => 1.0}
 end
 
 get '/' do
@@ -40,15 +40,16 @@ end
 
 # TODO: Figure out why occasionally it gives 'object object' to the browser
 get '/api/generators/species-select' do
-  # Convert string boolean arguments to real boolean arguments.
-  puts params[:social_statuses].to_s
   rarity_weight = params[:rarity_prefs].to_i
   galactic_location = params[:galactic_location].to_s
+  human_prefs = params[:human_prefs]
+    puts human_prefs
+    puts human_prefs.class
   @chosen = []
   @all_species = generate_species(params[:social_statuses])
   for i in 0...params[:num_species].to_i
     species_probs = species_basic_distribution
-    @chosen << select_species({:human_prefs => params[:human_prefs], :rarity_weighting => rarity_weight, :region => galactic_location, :species_probs => species_probs})
+    @chosen << select_species({:human_prefs => human_prefs, :rarity_weighting => rarity_weight, :region => galactic_location, :species_probs => species_probs})
   end
   STDERR.puts @chosen.to_s
   content_type :json
@@ -62,7 +63,6 @@ def generate_species(social_status)
   # Parsing social status variables
   social_statuses = [:liked, :respected, :beloved, :enslaved, :denigrated, :feared, :hated, :neutral, :mysterious]
   is_social_freedom = social_status["0"]
-  puts is_social_freedom.to_s
   social_restriction_type = social_status["1"]
 
   # If there are no restrictions, return all the species
@@ -111,7 +111,6 @@ def select_species(preferences)
   subtotal = 0.0
   # Go through the list
   for i in 0...species_probabilities.length do
-    STDERR.puts("Seeing if number is between " << subtotal.to_s << " and " << (species_probabilities.values[i] + subtotal).to_s)
     range = subtotal...(subtotal + species_probabilities.values[i])
     if range.cover? rand_num
       return [species_probabilities.keys[i], @all_species.where(:name => species_probabilities.keys[i]).take().wiki_link]
@@ -160,11 +159,11 @@ def adjust_by_rarity(rarity_factor, species_probabilities, region, human_rarity)
   end
   # Set human rarity to 40% after the fact
   unless species_probabilities["Human"] == nil
-    if human_rarity == 2
+    if human_rarity == '2'
   #    puts "Multiplying human rarity by " + (total * 2.0/3.0).to_s + " of a possible " + total.to_s
       species_probabilities["Human"] *= total*(2.0/3.0)
     # Set humans to Twi'lek rarity if that's the setting selected
-    elsif human_rarity == 1
+    elsif human_rarity == '1'
       species_probabilities["Human"] *= @rarity_coefficients["Ubiquitous"]**(rarity_factor/3.0)
     # Set humans to 0 rarity otherwise
     else
